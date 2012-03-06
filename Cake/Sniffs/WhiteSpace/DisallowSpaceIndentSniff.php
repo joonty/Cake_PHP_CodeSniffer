2f6328c9ef479841988effbe6e300db6c96c129f
Jon Cairns <jon@joncairns.com>
2012-03-06 17:17:28 +0000

    Added Tab sniff (space disallowed)

---
+++ Cake/Sniffs/WhiteSpace/DisallowSpaceIndentSniff.php
@@ -0,0 +1,92 @@
+<?php
+/**
+ * Generic_Sniffs_WhiteSpace_DisallowSpaceIndentSniff.
+ *
+ * PHP version 5
+ *
+ * @category  PHP
+ * @package   PHP_CodeSniffer
+ * @author    Greg Sherwood <gsherwood@squiz.net>
+ * @author    Marc McIntyre <mmcintyre@squiz.net>
+ * @copyright 2006-2011 Squiz Pty Ltd (ABN 77 084 670 600)
+ * @license   http://matrix.squiz.net/developer/tools/php_cs/licence BSD Licence
+ * @link      http://pear.php.net/package/PHP_CodeSniffer
+ */
+
+/**
+ * Generic_Sniffs_WhiteSpace_DisallowSpaceIndentSniff.
+ *
+ * Throws errors if tabs are used for indentation.
+ *
+ * @category  PHP
+ * @package   PHP_CodeSniffer
+ * @author    Greg Sherwood <gsherwood@squiz.net>
+ * @author    Marc McIntyre <mmcintyre@squiz.net>
+ * @copyright 2006-2011 Squiz Pty Ltd (ABN 77 084 670 600)
+ * @license   http://matrix.squiz.net/developer/tools/php_cs/licence BSD Licence
+ * @version   Release: @package_version@
+ * @link      http://pear.php.net/package/PHP_CodeSniffer
+ */
+class Cake_Sniffs_WhiteSpace_DisallowSpaceIndentSniff implements PHP_CodeSniffer_Sniff
+{
+
+    /**
+     * A list of tokenizers this sniff supports.
+     *
+     * @var array
+     */
+    public $supportedTokenizers = array(
+                                   'PHP',
+                                   'JS',
+                                   'CSS',
+                                  );
+
+
+    /**
+     * Returns an array of tokens this test wants to listen for.
+     *
+     * @return array
+     */
+    public function register()
+    {
+        return array(T_WHITESPACE);
+
+    }//end register()
+
+
+    /**
+     * Processes this test, when one of its tokens is encountered.
+     *
+     * @param PHP_CodeSniffer_File $phpcsFile All the tokens found in the document.
+     * @param int                  $stackPtr  The position of the current token in
+     *                                        the stack passed in $tokens.
+     *
+     * @return void
+     */
+    public function process(PHP_CodeSniffer_File $phpcsFile, $stackPtr)
+    {
+        $tokens = $phpcsFile->getTokens();
+
+        // Make sure this is whitespace used for indentation.
+        $line = $tokens[$stackPtr]['line'];
+        if ($stackPtr > 0 && $tokens[($stackPtr - 1)]['line'] === $line) {
+            return;
+        }
+	//var_dump($tokens[$stackPtr]);
+//	$content = $tokens[$stackPtr]['content'];
+//	for ($i=0; $i<strlen($content);$i++) {
+//
+//		echo ord($content[$i]). " ";
+//	}
+//	echo PHP_EOL;
+        if ($tokens[$stackPtr]['type'] == 'T_WHITESPACE' && ord($tokens[$stackPtr]['content'][0]) == 32) {
+            $error = 'Tabs must be used to indent lines; spaces are not allowed';
+            $phpcsFile->addError($error, $stackPtr, 'SpacesUsed');
+        }
+
+    }//end process()
+
+
+}//end class
+
+?>
